152f28fbb9eecd606511b629d0f382814b6bd1a3
emrahsenturk <emrahsenturkk@gmail.com>
2019-02-10 19:02:14 +0300

    Anasayfa tasarımı

--- src/app/drinkWater/drinkWater.module.ts
+++ src/app/drinkWater/drinkWater.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DrinkWaterPage } from './drinkWater.page';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ChartsModule,
     RouterModule.forChild([{ path: '', component: DrinkWaterPage }])
   ],
   declarations: [DrinkWaterPage]
--- src/app/drinkWater/drinkWater.page.ts
+++ src/app/drinkWater/drinkWater.page.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-drinkWater',
   templateUrl: 'drinkWater.page.html',
   styleUrls: ['drinkWater.page.scss']
 })
-export class DrinkWaterPage {}
+export class DrinkWaterPage {
+
+  @ViewChild('doughnutCanvas') doughnutCanvas;
+
+  doughnutChart: any;
+
+  ngOnInit(){
+
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+          labels: ["Kalan", "İçilen"],
+          datasets: [{
+              label: '# of Votes',
+              data: [850, 1750],
+              backgroundColor: [
+                  '#FF6384',
+                  '#36A2EB'
+              ],
+              hoverBackgroundColor: [
+                  "rgba(255, 99, 132, 0.2)",
+                  "rgba(54, 162, 235, 0.2)"
+              ]
+          }]
+      }
+    });
+  }
+
+}
